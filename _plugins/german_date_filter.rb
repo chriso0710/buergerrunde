6abaac4ce397057e3048c0b466d1887d9126784e
Christian Ott <mail@christian.ott.name>
2024-03-05 21:26:54 +0000

    farben; menu config

--- _plugins/german_date_filter.rb
+++ _plugins/german_date_filter.rb
@@ -1,5 +1,6 @@
 module DateFilter
     MONTHS = %w(Januar Februar März April Mai Juni Juli August September Oktober November Dezember)
+    
     def german_date(input)
         "#{input.strftime("%-d")}. #{MONTHS[input.strftime("%m").to_i - 1]} #{input.strftime("%Y")}"
     end
